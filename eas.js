878dab32d6a6344d25fd8b480d79f7003e7a2df4
ziadgamall <ziadgamal497@gmail.com>
2023-09-16 19:26:04 +0300

    Edit black, erase and reset functions Add randomColor functions
    black, erase and reset change the background color of the cell directly
    instead of changing the class.
    randomColor changes the background color of the hovered over cell randomly.

--- eas.js
+++ eas.js
@@ -1,31 +1,45 @@
 const container = document.getElementById('container');
 const columns = document.getElementById('column-container');
-
-for (let j = 1; j < 17; j++) {
+const n = 20
+    //creating a 16x16 grid
+for (let j = 0; j < n; j++) {
 
     const column = document.createElement('div');
     column.classList.add('column')
     
-    for (let i = 1; i < 17; i++) {
+    for (let i = 0; i < n; i++) {
         const row = document.createElement('div')
         row.classList.add('row')
         column.appendChild(row)
     } 
-    container.appendChild(column)
+    columns.appendChild(column)
 }
 
 const cells = document.querySelectorAll('.row');
+const dark = document.getElementById('dark')
+const reset = document.getElementById('reset');
+const erase = document.getElementById('erase');
+const rainbow = document.getElementById('color')
 
-cells.forEach(cell => {cell.addEventListener('mouseover', () => changeColor(cell))})
+    //change the color of cells if hovered over (black)
+dark.addEventListener('click', () => cells.forEach(cell => {cell.addEventListener('mouseover', () => cell.style.backgroundColor = '#000')}));
 
-function changeColor (e) {
-    e.classList.add('dark')
-}
+    //make the color of all cells white
+reset.addEventListener('click', () => {removeClass(cells)});
 
-const reset = document.getElementById('reset')
-
-reset.addEventListener('click', () => {removeClass(cells)})
-
+    //function responsible for turning the cells white
 function removeClass (cells) {
-    cells.forEach(cell => cell.classList.remove('dark'))
+    cells.forEach(cell => cell.style.backgroundColor = "#fff");
 }
+
+    //make the color of the hovered over cell white
+erase.addEventListener('click', () => cells.forEach(cell => {cell.addEventListener('mouseover', () => cell.style.backgroundColor = '#fff')}));
+
+    //generate a random color
+function randomColor (e) {
+    let random = '#' + Math.floor(Math.random()*16777215).toString(16);
+    e.style.backgroundColor = random;
+    console.log(random)
+}
+
+rainbow.addEventListener('click', () => cells.forEach(cell => {cell.addEventListener('mouseover', () => randomColor(cell))}));
